d4ca92c5325117918119d454a34fb56947a6d0c9
Anshita Bhasin <anshita.bhasin@gmail.com>
2025-04-06 13:22:08 +0400

    objects & classes - arrow function || examples

--- practiceQuestions_Objects.js
+++ practiceQuestions_Objects.js
@@ -1,18 +1,33 @@
 const bike = {
     brand: "Ducati",
 
-    logBrandRegular: function () {
-        setTimeout(function () {
-            console.log("Regular Timeout:", this.brand);
-        }, 1000);
+    logBrandRegular: function () { 
+            console.log("Object - Regular Timeout:", this.brand);
     },
 
-    logBrandArrow: function () {
-        setTimeout(() => {
-            console.log("Arrow Timeout:", this.brand);
-        }, 1000);
+    logBrandArrow:  ()=> {
+            console.log("Object - Arrow Timeout:", this.brand);
     }
 };
 
 bike.logBrandRegular(); //  "Regular Timeout: undefined"
 bike.logBrandArrow();   // "Arrow Timeout: Ducati"
+
+// Inside Class - Example 
+
+const car = {
+    brand: "BMW",
+
+    // Regular function
+    showBrand: function () {
+        console.log("Class - Regular Function:", this.brand);
+    },
+
+    // Arrow function
+    showArrowBrand: () => {
+        console.log("Class - Arrow Function:", this.brand);
+    }
+};
+
+car.showBrand();       // "Regular Function: BMW"
+car.showArrowBrand();  // "Arrow Function: undefined"
